e700b6255e32cee4cc06651630d554ceeb576d2e
Bushra Shakeel <aasiyabushra335@gmail.com>
2024-01-10 23:20:44 +0500

    qq

---
+++ data.js
@@ -0,0 +1,14 @@
+var organization = [
+    {
+        "id":0,
+        "name":"abc",
+        "number":5677,
+        "describtion":"abcderffffff",
+    },
+    {
+        "id":1,
+        "name":"bbc",
+        "number":5677,
+        "describtion":"abcderffffff",
+    },
+    ]
